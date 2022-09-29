26e290884b1729c3dabf399bf2e2d7d26dc9285b
callmesiva <sgnathan10@gmail.com>
2022-09-29 10:39:08 +0530

    Need updates

---
+++ main.js
@@ -0,0 +1,84 @@
+
+//Promise
+
+// console.log("person1: shows ticket")
+// console.log("person2: shows ticket")
+
+// let wifeBringTickets = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('ticket')
+//     }, 2000)
+// })
+
+// let getpop = wifeBringTickets.then((value) => {
+
+//     console.log("Husband: we should go in");
+//     console.log("wife: no i am hungry ");
+//     console.log("Husband: I got some popcorn")
+//     return new Promise((resolve, reject) => resolve(`${value} popcorn`))
+// })
+
+// let butter = getpop.then((value) => {
+//     console.log("Husband: we should go in")
+//     console.log("wife: i want butter on it")
+//     return new Promise((resolve, reject) => resolve(`${value} butter`))
+
+
+// })
+
+// butter.then((value) => {
+//     console.log(value);
+// })
+
+// console.log("person4: shows ticket")
+// console.log("person5: shows ticket")
+
+
+//Async Await
+
+console.log("person1: shows ticket")
+console.log("person2: shows ticket")
+
+let premovie = async () => {
+
+    let wifeBringTickets = new Promise((resolve, reject) => {
+        setTimeout(() => resolve('ticket'), 2000)
+    })
+
+    let getpop = new Promise((resolve, reject) => resolve("Popcorn"));
+    let addbutter = new Promise((resolve, reject) => resolve("Butter"));
+    let getcolddrink = new Promise((resolve, reject) => resolve("Cold Drink"))
+
+    let ticket = await wifeBringTickets;
+
+    console.log(`Wife: I have ${ticket}`);
+    console.log("Husband: we should go in");
+    console.log("wife: no im hungry");
+
+    let pop = await getpop;
+
+    console.log(`Husband: I got some ${pop}`);
+    console.log("Husband: we should go in");
+    console.log("wife: i need butter on it");
+
+    let butter = await addbutter;
+
+    console.log(`husaband: I got some ${butter} on popcorn`);
+    console.log("wife: lets go already late");
+    console.log("Husband: just minute I get some Drink")
+
+    let Colddrink = await getcolddrink;
+
+    console.log(`wife: did you get a ${Colddrink}`)
+    console.log("Yeah lets go")
+
+    return ticket
+}
+
+premovie().then((value) => {
+    console.log(`person3: show ${value}`);
+})
+
+
+console.log("person4: shows ticket")
+console.log("person5: shows ticket")
